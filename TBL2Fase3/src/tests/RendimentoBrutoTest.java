2e84e823674254def4980730d0e3b06b87b6d88f
DanMke <danmke@hotmail.com>
2019-11-13 14:58:26 -0300

    Implementado teste de rendimento bruto em barra vermelha por falta de implementacao da unidade

---
+++ TBL2Fase3/src/tests/RendimentoBrutoTest.java
@@ -0,0 +1,16 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RendimentoBrutoTest {
+
+	@Test
+	public void testRendimentoBruto() {
+		// 60 dias, 1000 de aplicação inicial, 8.5 de taxa de juros anual, 22.5 aliquota
+		Aplicacao aplicacao = new Aplicacao(60, 1000f, 8.5f, 22.5f);
+		assertEquals(13.97f, aplicacao.calcularRendimentoBruto(), 0.01f);
+	}
+
+}
