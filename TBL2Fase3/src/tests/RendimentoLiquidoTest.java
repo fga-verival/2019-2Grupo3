ec97847b9f2fe9a226e40f265eca76a7134e75ca
DanMke <danmke@hotmail.com>
2019-11-13 16:11:15 -0300

    Obtendo barra verde apos 3o teste para testar o calculo do imposto de renda, triangulando e parametrizando

--- TBL2Fase3/src/tests/RendimentoLiquidoTest.java
+++ TBL2Fase3/src/tests/RendimentoLiquidoTest.java
@@ -2,22 +2,52 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import application.Aplicacao;
 
+@RunWith(Parameterized.class)
 public class RendimentoLiquidoTest {
-
-	@Test
-	public void testRendimentoLiquido() {
-		Aplicacao aplicacao = new Aplicacao(60, 1000f, 8.5f, 22.5f);
-		assertEquals(1.0829f, aplicacao.calcularRendimentoLiquido(), 0.01f);
+	
+	private Integer qtdDias;
+	private Float aplicacaoInicial;
+	private Float taxaJurosAnual;
+	private Float aliquotaImpostoRenda;
+	private Float rendimentoLiquido;
+	
+	public RendimentoLiquidoTest(
+			Integer qtdDias, 
+			Float aplicacaoInicial, 
+			Float taxaJurosAnual, 
+			Float aliquotaImpostoRenda, 
+			Float rendimentoLiquido) {
+		this.qtdDias = qtdDias;
+		this.aplicacaoInicial = aplicacaoInicial;
+		this.taxaJurosAnual = taxaJurosAnual;
+		this.aliquotaImpostoRenda = aliquotaImpostoRenda;
+		this.rendimentoLiquido = rendimentoLiquido;
+	}
+	
+	@Parameters
+	public static Iterable<Object[]> getParameters() {
+		Object[][] res = new Object[][] {
+			{60, 1000f, 8.5f, 22.5f, 1.0829f},
+			{120, 500f, 8f, 22.5f, 2.0384f},
+			{240, 3000f, 9f, 20f, 4.7342f},
+			{1000, 100f, 7.5f, 15f, 17.4658f}
+		};
+		return Arrays.asList(res);
 	}
 	
 	@Test
-	public void testRendimentoLiquido2() {
-		Aplicacao aplicacao = new Aplicacao(120, 500f, 8f, 22.5f);
-		assertEquals(2.0384f, aplicacao.calcularRendimentoLiquido(), 0.01f);
+	public void testRendimentoLiquido() {
+		Aplicacao aplicacao = new Aplicacao(qtdDias, aplicacaoInicial, taxaJurosAnual, aliquotaImpostoRenda);
+		assertEquals(rendimentoLiquido, aplicacao.calcularRendimentoLiquido(), 0.01f);
 	}
 
 }
