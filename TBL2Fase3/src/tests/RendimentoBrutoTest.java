db837fab43fd8e2ec551cbd59278610dedc843ad
DanMke <danmke@hotmail.com>
2019-11-13 15:23:13 -0300

    Obtendo barra verde apos 3o teste e triangulacao e parametrizacao do teste de calcular rendimento bruto

--- TBL2Fase3/src/tests/RendimentoBrutoTest.java
+++ TBL2Fase3/src/tests/RendimentoBrutoTest.java
@@ -2,23 +2,52 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import application.Aplicacao;
 
+@RunWith(Parameterized.class)
 public class RendimentoBrutoTest {
-
-	@Test
-	public void testRendimentoBruto() {
-		// 60 dias, 1000 de aplicação inicial, 8.5 de taxa de juros anual, 22.5 aliquota
-		Aplicacao aplicacao = new Aplicacao(60, 1000f, 8.5f, 22.5f);
-		assertEquals(13.97f, aplicacao.calcularRendimentoBruto(), 0.01f);
+	
+	private Integer qtdDias;
+	private Float aplicacaoInicial;
+	private Float taxaJurosAnual;
+	private Float aliquotaImpostoRenda;
+	private Float rendimentoBruto;
+	
+	public RendimentoBrutoTest(
+			Integer qtdDias, 
+			Float aplicacaoInicial, 
+			Float taxaJurosAnual, 
+			Float aliquotaImpostoRenda, 
+			Float rendimentoBruto) {
+		this.qtdDias = qtdDias;
+		this.aplicacaoInicial = aplicacaoInicial;
+		this.taxaJurosAnual = taxaJurosAnual;
+		this.aliquotaImpostoRenda = aliquotaImpostoRenda;
+		this.rendimentoBruto = rendimentoBruto;
+	}
+	
+	@Parameters
+	public static Iterable<Object[]> getParameters() {
+		Object[][] res = new Object[][] {
+			{60, 1000f, 8.5f, 22.5f, 13.97f},
+			{120, 500f, 8f, 22.5f, 13.15f},
+			{240, 3000f, 9f, 20f, 177.53f},
+			{420, 250f, 8f, 17.5f, 23.01f}
+		};
+		return Arrays.asList(res);
 	}
 	
 	@Test
-	public void testRendimentoBruto2() {
-		Aplicacao aplicacao = new Aplicacao(120, 500f, 8f, 22.5f);
-		assertEquals(13.15f, aplicacao.calcularRendimentoBruto(), 0.01f);
+	public void testRendimentoBruto() {
+		Aplicacao aplicacao = new Aplicacao(qtdDias, aplicacaoInicial, taxaJurosAnual, aliquotaImpostoRenda);
+		assertEquals(rendimentoBruto, aplicacao.calcularRendimentoBruto(), 0.01f);
 	}
-
+	
 }
