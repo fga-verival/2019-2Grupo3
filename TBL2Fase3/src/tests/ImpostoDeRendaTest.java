08c5cb2c443df159e95f4b2fd8d8c92b286ae35f
DanMke <danmke@hotmail.com>
2019-11-13 15:41:47 -0300

    Obtendo barra vermelha por novo teste para calculo de imposto de renda

--- TBL2Fase3/src/tests/ImpostoDeRendaTest.java
+++ TBL2Fase3/src/tests/ImpostoDeRendaTest.java
@@ -13,5 +13,11 @@ public class ImpostoDeRendaTest {
 		Aplicacao aplicacao = new Aplicacao(60, 1000f, 8.5f, 22.5f);
 		assertEquals(3.14f, aplicacao.calcularImpostoDeRenda(), 0.01f);
 	}
+	
+	@Test
+	public void testImpostoRenda2() {
+		Aplicacao aplicacao = new Aplicacao(120, 500f, 8f, 22.5f);
+		assertEquals(2.96f, aplicacao.calcularImpostoDeRenda(), 0.01f);
+	}
 
 }
