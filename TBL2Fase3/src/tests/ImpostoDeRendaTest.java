304d2684f4eb65e20e42930b9bc977e86afbc54f
DanMke <danmke@hotmail.com>
2019-11-13 15:46:20 -0300

    Obtendo barra verde apos 3o teste escrito para testar calculo do imposto de renda, triangulacao e parametrizacao

--- TBL2Fase3/src/tests/ImpostoDeRendaTest.java
+++ TBL2Fase3/src/tests/ImpostoDeRendaTest.java
@@ -2,22 +2,51 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import application.Aplicacao;
 
+@RunWith(Parameterized.class)
 public class ImpostoDeRendaTest {
-
-	@Test
-	public void testImpostoRenda() {
-		Aplicacao aplicacao = new Aplicacao(60, 1000f, 8.5f, 22.5f);
-		assertEquals(3.14f, aplicacao.calcularImpostoDeRenda(), 0.01f);
+	
+	private Integer qtdDias;
+	private Float aplicacaoInicial;
+	private Float taxaJurosAnual;
+	private Float aliquotaImpostoRenda;
+	private Float impostoRenda;
+	
+	public ImpostoDeRendaTest(
+			Integer qtdDias, 
+			Float aplicacaoInicial, 
+			Float taxaJurosAnual, 
+			Float aliquotaImpostoRenda, 
+			Float impostoRenda) {
+		this.qtdDias = qtdDias;
+		this.aplicacaoInicial = aplicacaoInicial;
+		this.taxaJurosAnual = taxaJurosAnual;
+		this.aliquotaImpostoRenda = aliquotaImpostoRenda;
+		this.impostoRenda = impostoRenda;
+	}
+	
+	@Parameters
+	public static Iterable<Object[]> getParameters() {
+		Object[][] res = new Object[][] {
+			{120, 500f, 8f, 22.5f, 2.96f},
+			{270, 2000f, 8.5f, 20f, 25.15f},
+			{550, 400f, 8f, 17.5f, 8.44f}
+		};
+		return Arrays.asList(res);
 	}
 	
 	@Test
-	public void testImpostoRenda2() {
-		Aplicacao aplicacao = new Aplicacao(120, 500f, 8f, 22.5f);
-		assertEquals(2.96f, aplicacao.calcularImpostoDeRenda(), 0.01f);
+	public void testImpostoRenda() {
+		Aplicacao aplicacao = new Aplicacao(qtdDias, aplicacaoInicial, taxaJurosAnual, aliquotaImpostoRenda);
+		assertEquals(impostoRenda, aplicacao.calcularImpostoDeRenda(), 0.01f);
 	}
 
 }
